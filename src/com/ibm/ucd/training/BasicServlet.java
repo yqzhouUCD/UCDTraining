0457f9d1b16db6712a65dabb66ed14391c750426
zhangsword <sword_zhang@163.com>
2017-03-13 10:26:49 +0800

    rename warfile dir from bin to dist

--- src/com/ibm/ucd/training/BasicServlet.java
+++ src/com/ibm/ucd/training/BasicServlet.java
@@ -27,7 +27,7 @@ public class BasicServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Servlet message for UCD training!");
+		response.getWriter().append("Servlet message for UCD training V1.0!");
 	}
 
 	/**
